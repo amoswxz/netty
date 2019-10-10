27fea5509395087d849f0ef045ddd59d647227be
Amos <Amos.wxz@gmail.com>
2019-10-10 16:36:47 +0800

    #issues

--- example/src/main/java/io/netty/example/pimow/netty/A20190821/io/BioSocketClient.java
+++ example/src/main/java/io/netty/example/pimow/netty/A20190821/io/BioSocketClient.java
@@ -2,6 +2,7 @@ package io.netty.example.pimow.netty.A20190821.io;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -11,7 +12,7 @@ public class BioSocketClient {
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("0.0.0.0", 9595);
-        socket.getOutputStream().write("我想要回家".getBytes(StandardCharsets.UTF_8));
+        socket.getOutputStream().write("我想要回家".getBytes(Charset.defaultCharset()));
     }
 
 }
